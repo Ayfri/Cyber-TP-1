cb508794a022d646ce3d0b1b62ea68b7c160c5d0
Ayfri <pierre.ayfri@gmail.com>
2023-06-01 17:39:00 +0200

    feat(database): Add database connection.

---
+++ src/DatabaseConnection.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+use PDO;
+use function App\utils\env;
+
+/**
+ * Class DatabaseConnection is a class that represents a database connection
+ * @package Database
+ */
+class DatabaseConnection {
+	private ?PDO $database = null;
+
+	/**
+	 * getConnection is the function that gets the database connection
+	 * @return PDO - the database connection
+	 */
+	public function getConnection(): PDO {
+		if ($this->database === null) {
+			$host = env('DB_HOST');
+			$password = env('DB_PASSWORD');
+			$username = env('DB_USERNAME');
+			$port = env('DB_PORT');
+			$database = env('DB_NAME');
+
+			$this->database = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password,
+				[
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+					PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+				]);
+		}
+
+		return $this->database;
+	}
+}
--- src/utils/env.php
+++ src/utils/env.php
@@ -6,12 +6,12 @@ namespace App\utils;
 use Dotenv\Dotenv;
 
 function load_env(): void {
-	$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+	$dotenv = Dotenv::createImmutable(__DIR__ . '/../../../');
 	$dotenv->load();
 }
 
 function env(string $key, string $default = ''): string {
-	$value = getenv($key);
+	$value = getenv("CYBER_$key");
 	if ($value === false) {
 		return $default;
 	}
